eff64baa83a1cf5128b683558c7bdb43d2a7ebe8
unknown <jignasharajput.ca@silveroakuni.ac.in>
2022-06-14 17:45:56 +0530

    jsjquery

---
+++ day 6/js/jspro1.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+//alert("ready");
+// $("#hide").click(function(){
+// 	$("#details").hide();
+// });
+// $("#show").click(function()
+// {
+// 	$("#details").show();
+// })
+// });
+
+$("#toggle").click(function(){
+	$("#details").fadeToggle("slow");
+});
+});
+
